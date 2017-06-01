09eace4fcaff17b2f9f1b6d5d3df444233acbf8d
Kuntal Ghosh <kuntal.ghosh@enterprisedb.com>
2017-06-01 18:13:11 +0530

    Add a 'storage_engine' reloption.
    The code that decides which storage should be used for a relation.
    Allowed values are 'heap' and 'zheap'. The default value for this
    options is 'heap'. Also, it adds a macro 'RelationStorageIsZHeap(relation)'
    which can be used to decide whether a relation uses zheap or not.
    
    Patch by me, reviewed by Mithun C Y

--- src/backend/executor/nodeModifyTable.c
+++ src/backend/executor/nodeModifyTable.c
@@ -280,10 +280,17 @@ ExecInsert(ModifyTableState *mtstate,
 	OnConflictAction onconflict = node->onConflictAction;
 
 	/*
+	 * get information on the (current) result relation
+	 */
+	resultRelInfo = estate->es_result_relation_info;
+
+	resultRelationDesc = resultRelInfo->ri_RelationDesc;
+
+	/*
 	 * get the heap tuple out of the tuple table slot, making sure we have a
 	 * writable copy
 	 */
-	if (enable_zheap)
+	if (RelationStorageIsZHeap(resultRelationDesc))
 		ztuple = ExecMaterializeZSlot(slot);
 	else
 		tuple = ExecMaterializeSlot(slot);
@@ -386,7 +393,7 @@ ExecInsert(ModifyTableState *mtstate,
 		 * Constraints might reference the tableoid column, so initialize
 		 * t_tableOid before evaluating them.
 		 */
-		if (enable_zheap)
+		if (RelationStorageIsZHeap(resultRelationDesc))
 			ztuple->t_tableOid = RelationGetRelid(resultRelationDesc);
 		else
 			tuple->t_tableOid = RelationGetRelid(resultRelationDesc);
@@ -541,7 +548,7 @@ ExecInsert(ModifyTableState *mtstate,
 			 * Note: heap_insert returns the tid (location) of the new tuple
 			 * in the t_self field.
 			 */
-			if (enable_zheap)
+			if (RelationStorageIsZHeap(resultRelationDesc))
 				newId = zheap_insert(resultRelationDesc, ztuple,
 									 estate->es_output_cid,
 									 0);
@@ -562,7 +569,7 @@ ExecInsert(ModifyTableState *mtstate,
 	{
 		(estate->es_processed)++;
 		estate->es_lastoid = newId;
-		if (enable_zheap)
+		if (RelationStorageIsZHeap(resultRelationDesc))
 			setLastTid(&(ztuple->t_self));
 		else
 			setLastTid(&(tuple->t_self));
