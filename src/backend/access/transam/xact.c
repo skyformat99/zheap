184b1cbdb85e8e17bab88ae094a031b31483864b
dilip <dilip@localhost.localdomain>
2018-01-17 13:58:37 +0530

    Bug fix in rewind
    If we are under a subtransaction then just reuse one
    slot, because during the rollback of the subtransaction we will rewind the
    undo insert location and the undo written for invalidating the slot will be
    overwritten. So, it is better to invalidate only one slot which our
    transaction is going to use.
    
    Patch by Dilip Kumar reviewed by Amit Kapila

--- src/backend/access/transam/xact.c
+++ src/backend/access/transam/xact.c
@@ -3793,7 +3793,7 @@ UserAbortTransactionBlock(void)
 
 	/* execute the undo actions, but we should be inside current transaction */
 	if (latest_urec_ptr && (CurrentTransactionState->state == TRANS_INPROGRESS))
-		execute_undo_actions(latest_urec_ptr, s->start_urec_ptr, true, true);
+		execute_undo_actions(latest_urec_ptr, s->start_urec_ptr, true);
 }
 
 /*
@@ -4172,7 +4172,7 @@ RollbackToSavepoint(const char *name)
 
 	/* execute the undo actions */
 	if (latest_urec_ptr && (s->state == TRANS_INPROGRESS))
-		execute_undo_actions(latest_urec_ptr, xact->start_urec_ptr, false, true);
+		execute_undo_actions(latest_urec_ptr, xact->start_urec_ptr, false);
 }
 
 /*
