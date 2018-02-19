5d28a9548a31ff0356aed18bcba60c28e51007d1
Kuntal Ghosh <kuntal.ghosh@enterprisedb.com>
2018-02-19 21:18:29 +0530

    Fix zheap insert options for COPY
    Patch by me with help from Dilip Kumar

--- src/backend/access/zheap/zheapam.c
+++ src/backend/access/zheap/zheapam.c
@@ -6702,7 +6702,7 @@ reacquire_buffer:
 		 * Keep calm and put that on the page, and then as many other tuples
 		 * as fit.
 		 */
-		if (!(options & ZHTUP_SLOT_FROZEN))
+		if ((options & ZHTUP_SLOT_FROZEN) != ZHTUP_SLOT_FROZEN)
 			ZHeapTupleHeaderSetXactSlot(zheaptuples[ndone]->t_data, trans_slot_id);
 
 		/*
@@ -6779,7 +6779,7 @@ reacquire_buffer:
 				if (PageGetZHeapFreeSpace(page) < MAXALIGN(zheaptup->t_len) + saveFreeSpace)
 					break;
 
-				if (!(options & ZHTUP_SLOT_FROZEN))
+				if ((options & ZHTUP_SLOT_FROZEN) != ZHTUP_SLOT_FROZEN)
 					ZHeapTupleHeaderSetXactSlot(zheaptup->t_data, trans_slot_id);
 
 				RelationPutZHeapTuple(relation, buffer, zheaptup);
