a0bd2f219c7dcd22a33a30d2c353e21749d5804e
dilip <dilip@localhost.localdomain>
2018-03-02 01:17:23 -0800

    Commit (db9d0b2988c829b8e0599ebf087a10b98cb9690d) calculated the latest_urec_ptr in case of SUB_INPROGRESS but it should have done that for SUB_ABORT case.
    Pointed out by Rafia Sabih

--- src/backend/access/transam/xact.c
+++ src/backend/access/transam/xact.c
@@ -3691,9 +3691,6 @@ EndTransactionBlock(void)
 					elog(FATAL, "EndTransactionBlock: unexpected state %s",
 						 BlockStateAsString(s->blockState));
 				s = s->parent;
-
-				if (!UndoRecPtrIsValid(latest_urec_ptr))
-					latest_urec_ptr = s->latest_urec_ptr;
 			}
 			if (s->blockState == TBLOCK_INPROGRESS)
 				s->blockState = TBLOCK_END;
@@ -3719,6 +3716,9 @@ EndTransactionBlock(void)
 					elog(FATAL, "EndTransactionBlock: unexpected state %s",
 						 BlockStateAsString(s->blockState));
 				s = s->parent;
+
+				if (!UndoRecPtrIsValid(latest_urec_ptr))
+					latest_urec_ptr = s->latest_urec_ptr;
 			}
 			if (s->blockState == TBLOCK_INPROGRESS)
 				s->blockState = TBLOCK_ABORT_PENDING;
